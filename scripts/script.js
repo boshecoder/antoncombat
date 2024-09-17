7bead6e3ae55d40e9e6b783d7b7125b6ec6f88d8
boshecoder <cursedzxc@duck.com>
2024-09-17 13:09:40 +0500

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -2,9 +2,23 @@ const tap = document.getElementById("tap");
 const anth = document.querySelector('h2');
 const boost = document.getElementById("boost");
 const tapaud = new Audio("sounds/tap.mp3");
+const adc = document.getElementById("adc");
+const adl = document.getElementById("address");
+const adi = document.getElementById("adi");
+
 let ants = localStorage.getItem("ants");
 let boosts = localStorage.getItem("boosts");
 let boost_cost = localStorage.getItem("boost_cost");
+let adc_s = false;
+let addc = localStorage.getItem("addc");
+
+if (addc) {
+    addc = addc;
+} else {
+    addc = "Не назначен";
+}    
+
+adl.textContent = `TON Адрес: ${addc}`;
 
 if (ants) {
     ants = parseInt(ants);
@@ -26,6 +40,23 @@ if (boost_cost) {
 
 anth.textContent = `${ants} $ANTON`;
 boost.textContent = `Улучшить тап (${boost_cost} $ANTON)`
+
+adc.addEventListener('click', function() {
+    if (adc_s == false) {
+        adi.style.display = "block";
+        adl.style.display = "none";
+        adc.textContent = "Подтвердить";
+        adc_s = true;
+    } else {
+        addc = adi.value;
+        adl.style.display = "block";
+        adl.textContent = `TON Адрес: ${addc}`;
+        localStorage.setItem("addc", addc);
+        adi.style.display = "none";
+        adc.textContent = "Изменить";
+        adc_s = false;
+    }    
+});
 
 tap.addEventListener('click', function() {
     ants = parseInt(ants + 1 * boosts);
